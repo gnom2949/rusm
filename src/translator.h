7ae8d133ff18fea06b43829ed00d191213e91394
gnom2949 <sasasilaev27@gmail.com>
2026-06-13 17:17:03 +0500

    flags

--- src/translator.h
+++ src/translator.h
@@ -4,12 +4,13 @@
 typedef struct {
   const char *ru;
   const char *nasm;
+  int mode; // 1 - 32bit, 2 - 64bit, 3 - both
 } Translate;
 
 extern Translate keywords[];
 
-void parse_line(const char *line, FILE *out);
+void parse_line(const char *line, FILE *out, int mode);
 void clean_token(char *token);
-const char *translate_token(const char *token, char *buffer);
+const char *translate_token(const char *token, char *buffer, int current_mode);
 int contains_cyrillic(const char *str);
 #endif
